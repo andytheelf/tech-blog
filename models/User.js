7ed5b093088da67ca41eddd28ce441ba86d4b36a
andytheelf <ballew22@gmail.com>
2020-06-20 22:01:51 -0500

    retry launch

--- models/User.js
+++ models/User.js
@@ -1,38 +1,33 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {
-    // set up method to run on instance data (per user) to check password
+
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
-// define table columns and configuration
+//table columns and configuration
 User.init({
-    // define an id column
+
     id: {
+
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
     },
-    // define a username column
+
+    // define a username 
     username: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    twitter: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    github: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    // define an email column
+
+    // define an email 
     email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -41,6 +36,7 @@ User.init({
             isEmail: true
         }
     },
+
     // define a password column
     password: {
         type: DataTypes.STRING,
@@ -51,18 +47,15 @@ User.init({
     }
 }, {
     hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
         async beforeCreate(newUserData) {
             newUserData.password = await bcrypt.hash(newUserData.password, 10);
             return newUserData;
         },
-        // set up beforeUpdate lifecycle "hook" functionality
         async beforeUpdate(updatedUserData) {
             updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
             return updatedUserData;
         }
     },
-
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- models/index.js
+++ models/index.js
@@ -1,14 +1,43 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 const Comment = require('./Comment');
 
-//create associations
+// create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
+});
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'vote_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
 });
 
 Comment.belongsTo(User, {
@@ -25,6 +54,11 @@ User.hasMany(Comment, {
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
-});
+})
 
-module.exports = { User, Post, Comment };
+module.exports = {
+    User,
+    Post,
+    Vote,
+    Comment
+};

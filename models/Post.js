606d04479c951ebce509842a4d32d069d0703f3e
andytheelf <ballew22@gmail.com>
2020-06-21 20:34:47 -0500

    post fail fix

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
+class Post extends Model {}
+
+
 Post.init({
     id: {
         type: DataTypes.INTEGER,
